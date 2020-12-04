587ef80cb778e4d3e5faa683eb4652c337f80696
Simon Weller <s.meierbeck@gmail.com>
2020-12-04 08:59:50 +0100

    y2020/d4 - refactored

--- src/main/kotlin/y2020/d4.kt
+++ src/main/kotlin/y2020/d4.kt
@@ -1,7 +1,6 @@
 package y2020
 
 import inputLines
-import jdk.jshell.spi.ExecutionControl.*
 
 fun main() {
     val lines = inputLines(2020, 4)
@@ -13,12 +12,12 @@ fun main() {
 }
 
 class Passport(string: String) {
-    private val fields: Map<String, String> = string.split(" ").fold(mapOf()) { map, fieldString ->
+    private val fields: Map<Field, String> = string.split(" ").fold(mapOf()) { map, fieldString ->
         val (key, value) = fieldString.split(":")
-        map.plus(key to value)
+        map.plus(Field.valueOf(key.toUpperCase()) to value)
     }
 
-    private val requiredFields = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+    private val requiredFields = Field.values().toSet().minus(Field.CID)
 
     val hasAllRequiredFields: Boolean
         get() = requiredFields.all { this.has(it) }
@@ -26,25 +25,33 @@ class Passport(string: String) {
     val isValid: Boolean
         get() = requiredFields.all { this.hasValid(it) }
 
-    private fun has(fieldName: String): Boolean = fields.containsKey(fieldName)
+    private fun has(field: Field): Boolean = fields.containsKey(field)
 
-    private fun hasValid(fieldName: String): Boolean {
-        val value = fields[fieldName] ?: return false
-        return try {
-            when (fieldName) {
-                "byr" -> value.toInt() in 1920..2002
-                "iyr" -> value.toInt() in 2010..2020
-                "eyr" -> value.toInt() in 2020..2030
-                "hgt" ->
+    private fun hasValid(field: Field): Boolean = has(field) && field.isValid(fields.getValue(field))
+
+    enum class Field {
+        BYR,
+        IYR,
+        EYR,
+        HGT,
+        HCL,
+        ECL,
+        PID,
+        CID;
+
+        fun isValid(value: String): Boolean {
+            return when (this) {
+                BYR -> value.toInt() in 1920..2002
+                IYR -> value.toInt() in 2010..2020
+                EYR -> value.toInt() in 2020..2030
+                HGT ->
                     value.endsWith("cm") && value.length == 5 && value.substring(0, 3).toInt() in 150..193 ||
                             value.endsWith("in") && value.length == 4 && value.substring(0, 2).toInt() in 59..76
-                "hcl" -> "#[0-9|a-f]{6}".toRegex().matches(value)
-                "ecl" -> value in listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
-                "pid" -> "\\d{9}".toRegex().matches(value)
-                else -> throw NotImplementedException("Unknown field")
+                HCL -> "#[0-9|a-f]{6}".toRegex().matches(value)
+                ECL -> value in setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+                PID -> "\\d{9}".toRegex().matches(value)
+                CID -> true
             }
-        } catch (e: Exception) {
-            false
         }
     }
 }
