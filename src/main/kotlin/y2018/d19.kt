aaa691fdfd6a9cd33201333b88178c50d11293ed
Simon Weller <s.meierbeck@gmail.com>
2020-12-08 19:22:41 +0100

    y2018/d19/1 - encapsulation

--- src/main/kotlin/y2018/d19.kt
+++ src/main/kotlin/y2018/d19.kt
@@ -4,16 +4,31 @@ import inputLines
 
 fun main() {
     val lines = inputLines(2018, 19)
-    val instructionPointerRegister = lines.first().substringAfter(" ").toInt()
-    val instructions = lines.drop(1).map { line ->
-        val (opcode, a, b, c) = line.split(" ")
-        Instruction(Opcode.valueOf(opcode.toUpperCase()), a.toInt(), b.toInt(), c.toInt())
+    val ipRegister = lines.first().substringAfter(" ").toInt()
+    val instructions = lines.drop(1).map(::parseInstruction)
+    val result = Cpu(ipRegister).process(instructions)
+    println(result)
+}
+
+fun parseInstruction(line: String): Instruction {
+    val (opcode, a, b, c) = line.split(" ")
+    return Instruction(Opcode.valueOf(opcode.toUpperCase()), a.toInt(), b.toInt(), c.toInt())
+}
+
+class Cpu (private val ipRegister: Int, initialValueFirstRegister: Long = 0) {
+    private var registers = Registers(mapOf(0 to initialValueFirstRegister))
+    private val ip: Int
+        get() = registers.register(ipRegister).toInt()
+
+    fun process(instructions: List<Instruction>): Long {
+        while(ip < instructions.size) {
+            process(instructions[ip])
+        }
+        return registers.values.getValue(0)
     }
 
-    var registers = Registers()
-    while (registers.register(instructionPointerRegister) < instructions.size) {
-        registers += instructions[registers.register(instructionPointerRegister).toInt()]
-        registers = Registers(registers.values.plus(instructionPointerRegister to registers.register(instructionPointerRegister) + 1))
+    private fun process(instruction: Instruction) {
+        registers += instruction
+        registers = Registers(registers.values.plus(ipRegister to registers.register(ipRegister) + 1))
     }
-    println(registers.register(0))
 }
