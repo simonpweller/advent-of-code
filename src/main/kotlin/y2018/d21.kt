cfc98d8d7a9d18084c681a4f962fa02448311eac
Simon Weller <s.meierbeck@gmail.com>
2020-12-10 00:04:24 +0100

    y2018/d21

--- src/main/kotlin/y2018/d21.kt
+++ src/main/kotlin/y2018/d21.kt
@@ -6,5 +6,35 @@ fun main() {
     val lines = inputLines(2018, 21)
     val ipRegister = lines.first().substringAfter(" ").toInt()
     val instructions = lines.drop(1).map(::parseInstruction)
-    Cpu(ipRegister, 0).process(instructions)
+    Cpu2(ipRegister, 0).process(instructions)
+}
+
+class Cpu2 (private val ipRegister: Int, initialValueFirstRegister: Long = 0) {
+    private var registers = Registers(mapOf(0 to initialValueFirstRegister))
+    private val ip: Int
+        get() = registers.register(ipRegister).toInt()
+
+    fun process(instructions: List<Instruction>): Long? {
+        val seen = mutableSetOf<Long>()
+        while(ip < instructions.size) {
+            if (ip == 18) { // short-circuit inner loop
+                registers = Registers(registers.values.plus(5 to registers.register(4) / 256).plus(2 to 26L).plus(1 to 1L))
+            }
+            if (ip == 28) { // would exit if register 0 == register 3
+                if (registers.register(3) in seen) {
+                    println(seen.first())
+                    println(seen.last())
+                    return null
+                }
+                seen.add(registers.register(3))
+            }
+            process(instructions[ip])
+        }
+        return registers.values.getValue(0)
+    }
+
+    private fun process(instruction: Instruction) {
+        registers += instruction
+        registers = Registers(registers.values.plus(ipRegister to registers.register(ipRegister) + 1))
+    }
 }
