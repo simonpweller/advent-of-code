1d774b4a896837826bb9b6fd0f387dcf72484ce7
Simon Weller <s.meierbeck@gmail.com>
2020-12-13 20:47:58 +0100

    y2020/d13 - refactored

--- src/main/kotlin/y2020/d13.kt
+++ src/main/kotlin/y2020/d13.kt
@@ -7,31 +7,29 @@ fun main() {
     val startingTimestamp = input.first().toInt()
 
     val busses = input[1].split(",").mapIndexedNotNull { index, busId ->
-        if (busId == "x") return@mapIndexedNotNull null
-        Bus(busId.toInt(), index)
+        if (busId == "x") null else Bus(busId.toLong(), index)
     }
 
     println(part1(busses, startingTimestamp))
     println(part2(busses))
 }
 
-private fun part1(busses: List<Bus>, startingTimestamp: Int): Int {
+private fun part1(busses: List<Bus>, startingTimestamp: Int): Long {
     val waitTimes = busses.map { bus -> WaitTime(bus.id, bus.id - (startingTimestamp % bus.id)) }
     return waitTimes.minByOrNull { it.waitTime }!!.let { it.busId * it.waitTime }
 }
 
 private fun part2(busses: List<Bus>): Long {
-    return (0..busses.lastIndex).fold(1L) { timestamp, index ->
-        val stepsize = busses.subList(0, index).fold(1L) { product, bus -> product * bus.id }
-        generateSequence(timestamp) { it + stepsize }.first {
-            if (index == 0) {
-                it % busses[index].id == 0L
-            } else {
-                it % busses[index].id == (busses[index].id - (busses[index].index % busses[index].id)).toLong()
-            }
+    var stepsize = busses.first().id
+    var timestamp = busses.first().id
+    busses.drop(1).forEach { bus ->
+        timestamp = generateSequence(timestamp) { it + stepsize }.first {
+            (it + bus.index) % bus.id == 0L
         }
+        stepsize *= bus.id
     }
+    return timestamp
 }
 
-private data class Bus(val id: Int, val index: Int)
-private data class WaitTime(val busId: Int, val waitTime: Int)
+private data class Bus(val id: Long, val index: Int)
+private data class WaitTime(val busId: Long, val waitTime: Long)
