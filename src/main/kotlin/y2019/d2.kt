25e14c59116d0caefef9f8b8eac1e623aa08d14e
Simon Weller <s.meierbeck@gmail.com>
2020-12-13 22:41:01 +0100

    y2019/d2/2

--- src/main/kotlin/y2019/d2.kt
+++ src/main/kotlin/y2019/d2.kt
@@ -3,16 +3,31 @@ package y2019
 import inputText
 
 fun main() {
-    val intcode = inputText(2019, 2).split(",").map(String::toInt).toMutableList()
-    intcode[1] = 12
-    intcode[2] = 2
-    (0..intcode.size - 4 step 4).forEach { index ->
-        val (opcode, arg1, arg2, target) = intcode.subList(index, index + 4)
-        when (opcode) {
-            1 -> intcode[target] = intcode[arg1] + intcode[arg2]
-            2 -> intcode[target] = intcode[arg1] * intcode[arg2]
-            99 -> println(intcode[0]).also { return }
+    val intcode = inputText(2019, 2).split(",").map(String::toInt)
+    println(part1(intcode, 12, 2))
+    println(part2(intcode))
+}
+
+private fun part2(intcode: List<Int>): Int {
+    (0 .. 99).forEach { noun ->
+        (0 .. 99).forEach { verb ->
+            if (part1(intcode, noun, verb) == 19690720) return 100 * noun + verb
         }
     }
+    throw IllegalArgumentException("No solution found")
+}
 
+private fun part1(intcode: List<Int>, noun: Int, verb: Int): Int {
+    val memory = intcode.toMutableList()
+    memory[1] = noun
+    memory[2] = verb
+    (0..memory.size - 4 step 4).forEach { index ->
+        val (opcode, arg1, arg2, target) = memory.subList(index, index + 4)
+        when (opcode) {
+            1 -> memory[target] = memory[arg1] + memory[arg2]
+            2 -> memory[target] = memory[arg1] * memory[arg2]
+            99 -> return memory[0]
+        }
+    }
+    throw IllegalArgumentException("Program didn't terminate")
 }
