f5a88b196fd62513e083837bf657399d624553fc
Simon Weller <s.meierbeck@gmail.com>
2020-12-22 07:34:00 +0100

    y2020/d22/2

--- src/main/kotlin/y2020/d22.kt
+++ src/main/kotlin/y2020/d22.kt
@@ -1,36 +1,71 @@
 package y2020
 
 import inputChunks
+import y2020.Player.*
 
 fun main() {
-    val (player1, player2) = inputChunks(2020, 22).map { chunk ->
-        val id = chunk.lines().first()[7].toInt()
-        val deck = chunk.lines().drop(1).map(String::toInt)
-        Player(id, deck)
+    val (deck1, deck2) = inputChunks(2020, 22).map { chunk ->
+        chunk.lines().drop(1).map(String::toInt)
     }
 
-    println(part1(player1, player2))
-    println(part2(player1, player2))
+    println(part1(deck1, deck2))
+    println(playGame(deck1, deck2).score)
 }
 
-private fun part1(player1: Player, player2: Player): Int {
-    while (player1.deck.isNotEmpty() && player2.deck.isNotEmpty()) {
-        val player1Card = player1.deck.first()
-        val player2Card = player2.deck.first()
-        if (player1Card > player2Card) {
-            player1.deck = player1.deck.drop(1).plus(player1Card).plus(player2Card)
-            player2.deck = player2.deck.drop(1)
+private fun part1(startingDeck1: List<Int>, staringDeck2: List<Int>): Int {
+    var deck1 = startingDeck1
+    var deck2 = staringDeck2
+    while (deck1.isNotEmpty() && deck2.isNotEmpty()) {
+        val card1 = deck1.first()
+        val card2 = deck2.first()
+        if (card1 > card2) {
+            deck1 = deck1.drop(1).plus(card1).plus(card2)
+            deck2 = deck2.drop(1)
         } else {
-            player1.deck = player1.deck.drop(1)
-            player2.deck = player2.deck.drop(1).plus(player2Card).plus(player1Card)
+            deck1 = deck1.drop(1)
+            deck2 = deck2.drop(1).plus(card2).plus(card1)
         }
     }
 
-    return player1.deck.plus(player2.deck).reversed().mapIndexed { index, card -> card * (index + 1) }.sum()
+    return if (deck1.isNotEmpty()) scoreDeck(deck1) else scoreDeck(deck2)
 }
 
-private fun part2(player1: Player, player2: Player): Int {
-    return 0
+private fun playGame(startingDeck1: List<Int>, staringDeck2: List<Int>): GameResult {
+    var deck1 = startingDeck1
+    var deck2 = staringDeck2
+
+    val seen = mutableSetOf<Pair<List<Int>, List<Int>>>()
+
+    while (deck1.isNotEmpty() && deck2.isNotEmpty()) {
+        if (Pair(deck1, deck2) in seen) {
+            return GameResult(PLAYER_1, scoreDeck(deck1))
+        } else {
+            seen.add(Pair(deck1, deck2))
+            val card1 = deck1.first()
+            val card2 = deck2.first()
+
+            val roundWinner: Player = when {
+                deck1.size > card1 && deck2.size > card2 -> {
+                    playGame(deck1.drop(1).take(card1), deck2.drop(1).take(card2)).winner // recursive case
+                }
+                else -> {
+                    if (card1 > card2) PLAYER_1 else PLAYER_2 // base case
+                }
+            }
+
+            if (roundWinner == PLAYER_1) {
+                deck1 = deck1.drop(1).plus(card1).plus(card2)
+                deck2 = deck2.drop(1)
+            } else {
+                deck1 = deck1.drop(1)
+                deck2 = deck2.drop(1).plus(card2).plus(card1)
+            }
+        }
+    }
+    return if (deck1.isNotEmpty()) GameResult(PLAYER_1,scoreDeck(deck1)) else GameResult(PLAYER_2, scoreDeck(deck2))
 }
 
-data class Player(val id: Int, var deck: List<Int>)
+private fun scoreDeck(deck: List<Int>): Int = deck.reversed().mapIndexed { index, card -> card * (index + 1) }.sum()
+
+private data class GameResult(val winner: Player, val score: Int)
+private enum class Player { PLAYER_1, PLAYER_2 }
