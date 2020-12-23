0a966315aba8a7b283c4dc73c0960b97a8eac437
Simon Weller <s.meierbeck@gmail.com>
2020-12-23 06:18:50 +0100

    y2019/d18/1

--- src/main/kotlin/y2019/d18.kt
+++ src/main/kotlin/y2019/d18.kt
@@ -10,39 +10,34 @@ fun main() {
         }
     }.toMap()
 
-    val doorPositions = map.mapNotNull { if (it.value in ('A'..'Z')) it.value to it.key else null }.toMap()
     val keyPositions = map.mapNotNull { if (it.value in ('a'..'z')) it.value to it.key else null }.toMap()
-    val completePaths = mutableListOf<List<Char>>()
+    val startingPosition = map.entries.first { it.value == '@' }.key
+    val startingPoint = StartingPoint(startingPosition, emptySet())
+    val queue = PriorityQueue<Path>(compareBy { it.steps })
+    queue.add(Path(startingPoint, 0))
 
-    val queue = PriorityQueue<List<Char>>(compareBy { 1 })
-    queue.add(emptyList())
+    val pathsLengths = mutableListOf<Int>()
+    val startingPointsChecked = mutableSetOf<StartingPoint>()
+    var maxPathLengthReached = 0
+
     while (queue.isNotEmpty()) {
         val path = queue.poll()
-        val openedDoors = path.mapNotNull { doorPositions.getOrDefault(it.toUpperCase(), null) }
-        val usedKeys = path.map { keyPositions.getValue(it) }
-        val currentMap = map.plus(openedDoors.plus(usedKeys).map { it to '.' })
-        val keysInRange = keysInRange(currentMap)
-        if (keysInRange.isEmpty()) completePaths.add(path) else queue.addAll(keysInRange.map { path.plus(it) })
-    }
+        if (startingPointsChecked.contains(path.startingPoint)) continue
+        startingPointsChecked.add(path.startingPoint)
 
-    println(completePaths.size)
-
-    val steps = completePaths.map { path ->
-        var steps = 0
-        var startingPosition = map.filter { it.value == '@' }.keys.first()
-        val currentMap = map.toMutableMap()
-        path.forEach { key ->
-            steps += stepsTo(currentMap, keyPositions.getValue(key))
-            val doorPosition = doorPositions.getOrDefault(key.toUpperCase(), null)
-            if (doorPosition != null) currentMap[doorPosition] = '.'
-            currentMap[keyPositions.getValue(key)] = '.'
-            currentMap[startingPosition] = '.'
-            currentMap[keyPositions.getValue(key)] = '@'
-            startingPosition = keyPositions.getValue(key)
+        val nextPaths = distances(map, path.startingPoint).map {
+            Path(StartingPoint(keyPositions.getValue(it.key), path.startingPoint.openedDoors.plus(it.key.toUpperCase())), path.steps + it.value)
         }
-        steps
+
+        if (path.startingPoint.openedDoors.size == keyPositions.size) pathsLengths.add(path.steps)
+        if (path.startingPoint.openedDoors.size > maxPathLengthReached) {
+            maxPathLengthReached = path.startingPoint.openedDoors.size
+            println(maxPathLengthReached)
+        }
+
+        queue.addAll(nextPaths)
     }
-    println(steps.minOrNull())
+    println(pathsLengths.minOrNull())
 }
 
 private data class Point(val row: Int, val col: Int) {
@@ -50,33 +45,25 @@ private data class Point(val row: Int, val col: Int) {
         get() = listOf(Point(row - 1, col), Point(row + 1, col), Point(row, col - 1), Point(row, col + 1))
 }
 
-private fun keysInRange(map: Map<Point, Char>): List<Char> {
-    val startingPosition = map.filter { it.value == '@' }.keys.first()
-    val queue = PriorityQueue<Point>(compareBy { 1 })
-    queue.add(startingPosition)
-    val seen = mutableSetOf(startingPosition)
+private fun distances(map: Map<Point, Char>, startingPoint: StartingPoint): Map<Char, Int> {
+    val queue = PriorityQueue<State>(compareBy { it.steps })
+    queue.add(State(startingPoint.position, 0))
+    val seen = mutableSetOf(startingPoint.position)
+    val distancesTo = mutableMapOf<Char, Int>()
     while (queue.isNotEmpty()) {
-        val point = queue.poll()
-        val next = point.neighbours.filter { it !in seen && map[it] != '#' && map[it] !in ('A'..'Z') && map[it] !in ('a' .. 'z') }
-        seen.addAll(point.neighbours)
-        queue.addAll(next)
-    }
-    return seen.filter { map[it] in ('a'..'z') }.map { map.getValue(it) }
-}
-
-private fun stepsTo(map: Map<Point, Char>, to: Point): Int {
-    val startingPosition = map.filter { it.value == '@' }.keys.first()
-    val queue = PriorityQueue<State>(compareBy { 1 })
-    queue.add(State(startingPosition, 0))
-    val seen = mutableSetOf(startingPosition)
-    while (queue.peek().position != to) {
         val state = queue.poll()
-        val next = state.position.neighbours.filter { it !in seen && map[it] != '#' && map[it] !in ('A'..'Z') && map[it] !in ('a' .. 'z').minus(map[to]) }
+        val char = map.getValue(state.position)
+        if (char in ('a' .. 'z') && distancesTo[char] == null) {
+            distancesTo[char] = state.steps
+        }
+        val next = state.position.neighbours.filter { it !in seen && map[it] != '#' && map[it] !in ('A'..'Z').minus(startingPoint.openedDoors) }
         seen.addAll(state.position.neighbours)
         queue.addAll(next.map { State(it, state.steps + 1) })
     }
-    return queue.poll().steps
+    return distancesTo
 }
 
+private data class StartingPoint(val position: Point, val openedDoors: Set<Char>)
 private data class State(val position: Point, val steps: Int)
-
+private data class Path(val startingPoint: StartingPoint, val steps: Int)
+private data class DistanceSpec(val points: Set<Point>, val openedDoors: Set<Char>)
