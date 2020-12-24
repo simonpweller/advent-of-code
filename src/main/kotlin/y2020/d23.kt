cdc3b1fcc1eeff798238892bc9eb50b60d196cfe
Simon Weller <s.meierbeck@gmail.com>
2020-12-24 06:34:37 +0100

    y2020/d23/2

--- src/main/kotlin/y2020/d23.kt
+++ src/main/kotlin/y2020/d23.kt
@@ -4,8 +4,50 @@ import inputText
 
 fun main() {
     val input = inputText(2020, 23).map { it.toString().toInt() }
+    println(part1(input))
+
+    val cup1 = play(input.plus(10 .. 1_000_000), 10_000_000).getValue(1)
+    println(cup1.next.label.toLong() * cup1.next.next.label)
+}
+
+private fun part1(input: List<Int>): String {
+    val cupMap = play(input, 100)
+    var current = cupMap.getValue(1)
+    val values = mutableListOf<Int>()
+    do {
+        values.add(current.next.label)
+        current = current.next
+    } while (current.label != 1)
+    return values.joinToString("")
+}
+
+private fun play(input: List<Int>, rounds: Int): Map<Int, Cup> {
     val cupMap = linkCups(input)
-    println(cupMap)
+    var currentCup = cupMap.getValue(input.first())
+    val maxLabel = cupMap.keys.maxOrNull() ?: throw Exception()
+
+    repeat(rounds) {
+        val cup1 = currentCup.next
+        val cup2 = cup1.next
+        val cup3 = cup2.next
+        currentCup.next = cup3.next
+
+        var destinationCupLabel = currentCup.label - 1
+        if (destinationCupLabel < 1) destinationCupLabel = maxLabel
+        while (destinationCupLabel in listOf(cup1.label, cup2.label, cup3.label)) {
+            destinationCupLabel--
+            if (destinationCupLabel < 1) destinationCupLabel = maxLabel
+        }
+
+        val destinationCup = cupMap.getValue(destinationCupLabel)
+        val destinationCupNext = destinationCup.next
+        destinationCup.next = cup1
+        cup3.next = destinationCupNext
+
+        currentCup = currentCup.next
+    }
+
+    return cupMap
 }
 
 private fun linkCups(input: List<Int>): Map<Int, Cup> {
