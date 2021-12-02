d367506a051776c77264b540cac8c628a1fb1e75
Simon Weller <s.meierbeck@gmail.com>
2021-12-02 09:49:00 +0100

    updated to kotlin 1.6, junit 5.8.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.6.0"
 }
 group = "me.smeie"
 version = "1.0-SNAPSHOT"
@@ -10,8 +10,8 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 tasks {
     compileKotlin {
