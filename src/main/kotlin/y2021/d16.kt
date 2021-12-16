e659ef3ea81f598ef1f1470cad5c7d95095079ed
Simon Weller <s.meierbeck@gmail.com>
2021-12-16 13:12:25 +0100

    y2021 d16 - tidy up

--- src/main/kotlin/y2021/d16.kt
+++ src/main/kotlin/y2021/d16.kt
@@ -6,20 +6,18 @@ import java.util.Collections.min
 
 fun main() {
     val binary = inputText(2021, 16).toBinary()
-    val topLevelPacket = parse(binary).first
+    val topLevelPacket = parse(binary).packet
     println(topLevelPacket.totalVersionIds)
     println(topLevelPacket.value)
 }
 
-private fun parse(binary: String): Pair<Packet, String> {
+private fun parse(binary: String): ParseResult {
     var remaining = binary
 
     val version = remaining.take(3).toInt(2).also { remaining = remaining.drop(3) }
     val typeId = remaining.take(3).toInt(2).also { remaining = remaining.drop(3) }
-    val subPackets = mutableListOf<Packet>()
-    var literal: Long? = null
 
-    if (typeId == 4) { // literal value
+    if (typeId == 4) {
         var tempNumber = ""
         while (remaining.first() == '1') {
             tempNumber += remaining.drop(1).take(4)
@@ -27,8 +25,9 @@ private fun parse(binary: String): Pair<Packet, String> {
         }
         tempNumber += remaining.drop(1).take(4)
         remaining = remaining.drop(5)
-        literal = tempNumber.toLong(2)
+        return ParseResult(LiteralPacket(version, typeId, tempNumber.toLong(2)), remaining)
     } else {
+        val subPackets = mutableListOf<Packet>()
         val lengthType = remaining.take(1).toInt(2).also { remaining = remaining.drop(1) }
         if (lengthType == 0) {
             val length = remaining.take(15).toInt(2).also { remaining = remaining.drop(15) }
@@ -46,30 +45,39 @@ private fun parse(binary: String): Pair<Packet, String> {
                 subPackets.add(subPacket)
             }
         }
+        return ParseResult(OperatorPacket(version, typeId, subPackets), remaining)
     }
-
-    return Pair(Packet(version, typeId, subPackets, literal), remaining)
 }
 
-private data class Packet(val version: Int, val typeId: Int, val subPackets: List<Packet>, val literal: Long?) {
+private data class ParseResult(val packet: Packet, val remaining: String)
+
+private interface Packet {
     val totalVersionIds: Int
-        get() = version + subPackets.sumOf { it.totalVersionIds }
     val value: Long
+}
+
+private data class LiteralPacket(val version: Int, val typeId: Int, override val value: Long): Packet {
+    override val totalVersionIds: Int
+        get() = version
+}
+
+private data class OperatorPacket(val version: Int, val typeId: Int, val subPackets: List<Packet>): Packet {
+    override val totalVersionIds: Int
+        get() = version + subPackets.sumOf { it.totalVersionIds }
+    override val value: Long
         get() {
             return when(typeId) {
                 0 -> this.subPackets.map { it.value }.reduce { a, b -> a + b}
                 1 -> this.subPackets.map { it.value }.reduce { a, b -> a * b}
                 2 -> min(this.subPackets.map { it.value })
                 3 -> max(this.subPackets.map { it.value })
-                4 -> this.literal!!
                 5 -> if (this.subPackets.first().value > this.subPackets.last().value) 1 else 0
                 6 -> if (this.subPackets.first().value < this.subPackets.last().value) 1 else 0
                 7 -> if (this.subPackets.first().value == this.subPackets.last().value) 1 else 0
-                else -> throw IllegalArgumentException("Unknown typeId $typeId")
+                else -> throw IllegalArgumentException("Unknown operator $typeId")
             }
         }
 }
+
 private fun Char.toBinary() = this.toString().toInt(16).toString(2).padStart(4, '0')
 private fun String.toBinary() = this.map(Char::toBinary).joinToString("")
-
-
