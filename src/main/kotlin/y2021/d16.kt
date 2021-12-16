7d8953a451ff652efc9ce325ae5d9142bf4968c7
Simon Weller <s.meierbeck@gmail.com>
2021-12-16 12:57:20 +0100

    y2021 d16 - part 2

--- src/main/kotlin/y2021/d16.kt
+++ src/main/kotlin/y2021/d16.kt
@@ -1,11 +1,14 @@
 package y2021
 
 import inputText
+import java.util.Collections.max
+import java.util.Collections.min
 
 fun main() {
     val binary = inputText(2021, 16).toBinary()
     val topLevelPacket = parse(binary).first
     println(topLevelPacket.totalVersionIds)
+    println(topLevelPacket.value)
 }
 
 private fun parse(binary: String): Pair<Packet, String> {
@@ -14,13 +17,17 @@ private fun parse(binary: String): Pair<Packet, String> {
     val version = remaining.take(3).toInt(2).also { remaining = remaining.drop(3) }
     val typeId = remaining.take(3).toInt(2).also { remaining = remaining.drop(3) }
     val subPackets = mutableListOf<Packet>()
-
+    var literal: Long? = null
 
     if (typeId == 4) { // literal value
+        var tempNumber = ""
         while (remaining.first() == '1') {
+            tempNumber += remaining.drop(1).take(4)
             remaining = remaining.drop(5)
         }
-        remaining = remaining.drop(5) // last group
+        tempNumber += remaining.drop(1).take(4)
+        remaining = remaining.drop(5)
+        literal = tempNumber.toLong(2)
     } else {
         val lengthType = remaining.take(1).toInt(2).also { remaining = remaining.drop(1) }
         if (lengthType == 0) {
@@ -41,12 +48,26 @@ private fun parse(binary: String): Pair<Packet, String> {
         }
     }
 
-    return Pair(Packet(version, typeId, subPackets), remaining)
+    return Pair(Packet(version, typeId, subPackets, literal), remaining)
 }
 
-private data class Packet(val version: Int, val typeId: Int, val subPackets: List<Packet>) {
+private data class Packet(val version: Int, val typeId: Int, val subPackets: List<Packet>, val literal: Long?) {
     val totalVersionIds: Int
         get() = version + subPackets.sumOf { it.totalVersionIds }
+    val value: Long
+        get() {
+            return when(typeId) {
+                0 -> this.subPackets.map { it.value }.reduce { a, b -> a + b}
+                1 -> this.subPackets.map { it.value }.reduce { a, b -> a * b}
+                2 -> min(this.subPackets.map { it.value })
+                3 -> max(this.subPackets.map { it.value })
+                4 -> this.literal!!
+                5 -> if (this.subPackets.first().value > this.subPackets.last().value) 1 else 0
+                6 -> if (this.subPackets.first().value < this.subPackets.last().value) 1 else 0
+                7 -> if (this.subPackets.first().value == this.subPackets.last().value) 1 else 0
+                else -> throw IllegalArgumentException("Unknown typeId $typeId")
+            }
+        }
 }
 private fun Char.toBinary() = this.toString().toInt(16).toString(2).padStart(4, '0')
 private fun String.toBinary() = this.map(Char::toBinary).joinToString("")
