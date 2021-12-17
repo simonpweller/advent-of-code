8564587a645a08fb31d9e2754faa44cf95badfe4
Simon Weller <s.meierbeck@gmail.com>
2021-12-17 12:58:23 +0100

    y2021 d17 - working solution

--- src/main/kotlin/y2021/d17.kt
+++ src/main/kotlin/y2021/d17.kt
@@ -1,41 +1,49 @@
 package y2021
 
 import inputText
+import kotlin.math.max
 
 fun main() {
     val input = inputText(2021, 17)
     val targetXRange = input.substringAfter("x=").substringBefore(",").let(::rangeStringToRange)
     val targetYRange = input.substringAfter("y=").let(::rangeStringToRange)
 
-    val xVelocityOptions = (0..targetXRange.last / 3)
-    val yVelocityOptions = (0..-targetYRange.first)
+    val xVelocityOptions = (0..targetXRange.last)
+    val yVelocityOptions = (targetYRange.first..-targetYRange.first)
 
-    val paths = (xVelocityOptions).flatMap { x -> yVelocityOptions.map { y -> path(x,  y, targetXRange, targetYRange) } }
-    println(paths.size)
+    val paths = (xVelocityOptions).flatMap { x -> yVelocityOptions.map { y -> getPath(x,  y, targetXRange, targetYRange) } }
     println(
         paths
-            .filter { it.steps.last().first in targetXRange && it.steps.last().second in targetYRange }
-            .maxOf { it.steps.maxOf { it.second } }
+            .filter { it.hits }
+            .maxOf { it.maxY }
+    )
+
+    println(
+        paths
+            .filter { it.hits }
+            .size
     )
 }
 
 private fun rangeStringToRange(rangeString: String): IntRange =
     rangeString.split("..").let { (it.first().toInt()..it.last().toInt()) }
 
-private fun path(xVelocity: Int, yVelocity: Int, targetXRange: IntRange, targetYRange: IntRange): Path {
-    return generateSequence(
-        Path(
-            listOf(Pair(0, 0)),
-            xVelocity,
-            yVelocity
-        )
-    ) {
-        Path(
-            it.steps + Pair(it.steps.last().first + it.xVelocity, it.steps.last().second + it.yVelocity),
-            adjustXVelocity(it.xVelocity),
-            it.yVelocity - 1,
-        )
-    }.takeWhile { it.steps.last().first <= targetXRange.last && it.steps.last().second >= targetYRange.last }.last()
+private fun getPath(initialXVelocity: Int, initialYVelocity: Int, targetXRange: IntRange, targetYRange: IntRange): Path {
+    var xVelocity = initialXVelocity
+    var yVelocity = initialYVelocity
+    var x = 0
+    var y = 0
+    var maxY = y
+    var hits = false
+    do {
+        x += xVelocity
+        y += yVelocity
+        maxY = max(maxY, y)
+        xVelocity = adjustXVelocity(xVelocity)
+        yVelocity--
+        if (x in targetXRange && y in targetYRange) hits = true
+    } while (x <= targetXRange.last && y >= targetYRange.first)
+    return Path(maxY, initialXVelocity, initialYVelocity, hits)
 }
 
 private fun adjustXVelocity(xVelocity: Int) = when {
@@ -44,4 +52,4 @@ private fun adjustXVelocity(xVelocity: Int) = when {
     else -> 0
 }
 
-private data class Path(val steps: List<Pair<Int, Int>>, val xVelocity: Int, val yVelocity: Int)
+private data class Path(val maxY: Int, val initialXVelocity: Int, val initialYVelocity: Int, val hits: Boolean)
